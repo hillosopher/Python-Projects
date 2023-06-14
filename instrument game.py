605a54e644f499614266e65200b14df895ee65e4
Josh Hill <joshuaamhill@gmail.com>
2023-06-14 16:05:42 -0700

    Had Chat GPT refactor my code

--- instrument game.py
+++ instrument game.py
@@ -1,4 +1,4 @@
-# Defines instrument class
+# Class Definitions
 class Instrument:
     material = None
     family = None
@@ -15,55 +15,44 @@ class Drum(Instrument):
     family = 'percussion'
     size = 'large'
 
-    def __str__(self):
-        info = f"Material: {self.material}\nFamily: {self.family}\nSize: {self.size}"
-        return info
-
     def playScale(self):
         msg = "You can't play a scale on a drum kit, silly!"
         print(msg)
 
 
-def getMsg():
+# Function to get non-empty input from user
+def get_non_empty_input(prompt):
+    while True:
+        user_input = input(prompt).strip()
+        if user_input:
+            return user_input
+        print("Oops, looks like you left it blank. Try again.")
 
-    def inst_msg():
-        inst = Instrument()
-        inst.tuning = scale
-        inst.playScale()
 
-    def play_again():
-        again = input("Would you like to play again? (Y/N)\n").lower()
-        if again == 'y':
-            print("Great!\n")
-            getMsg()
-        elif again == 'n':
-            print("Thanks for playing!")
+# Main function
+def main():
+    while True:
+        inst = get_non_empty_input(
+            "What instrument are you playing a scale with?\n").lower()
+
+        if inst == "drum":
+            drum = Drum()
+            drum.playScale()
         else:
+            scale = get_non_empty_input("What scale would you like to play?\n")
+            instrument = Instrument()
+            instrument.tuning = scale
+            instrument.playScale()
+
+        # Ask to play again
+        again = input("Would you like to play again? (Y/N)\n").lower()
+        if again == 'n':
+            print("Thanks for playing!")
+            break
+        elif again != 'y':
             print(
                 "Sorry! Type 'Y' if you'd like to play again, or 'N' if you're done playing.")
 
-    inst = input("What instrument are you playing a scale with?\n").lower()
-    if inst == "drum":
-        drum = Drum()
-        drum.playScale()
-        play_again()
-    else:
-        scale = input("What scale would you like to play?\n")
-        if scale == '':
-            go = True
-            while go:
-                scale = input(
-                    "Oops, looks like you left it blank. Try again.\nWhat scale would you like to play?\n")
-                if scale == '' or scale.isspace():
-                    go = True
-                else:
-                    go = False
-                    inst_msg()
-                    play_again()
-        else:
-            inst_msg()
-            play_again()
-
 
 if __name__ == "__main__":
-    getMsg()
+    main()
