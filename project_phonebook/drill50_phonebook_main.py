149117390f488236845241be3479485240ce8a9a
Josh Hill <joshuaamhill@gmail.com>
2023-06-17 09:38:06 -0700

    first commit of Phonebook project

--- project_phonebook/drill50_phonebook_main.py
+++ project_phonebook/drill50_phonebook_main.py
@@ -28,52 +28,59 @@
 # Tested OS:  This code was written and tested to work with Windows 10.
 
 
-from tkinter import *
-import tkinter as tk
-from tkinter import messagebox
-
-
-# Be sure to import our other modules 
+# Be sure to import our other modules
 # so we can have access to them
-import drill50_phonebook_gui
-import drill50_phonebook_func
 
 
 # Frame is the Tkinter frame class that our own class will inherit from
+
+
+
+
+from tkinter import *
+import tkinter as tk
+from tkinter import messagebox
+import drill50_phonebook_gui
+import drill50_phonebook_func
 class ParentWindow(Frame):
     def __init__(self, master, *args, **kwargs):
         Frame.__init__(self, master, *args, **kwargs)
 
         # define our master frame configuration
         self.master = master
-        self.master.minsize(500,300) #(Height, Width)
-        self.master.maxsize(500,300)
+        self.master.minsize(500, 300)  # (Height, Width)
+        self.master.maxsize(500, 300)
         # This CenterWindow method will center our app on the user's screen
-        drill50_phonebook_func.center_window(self,500,300)
+        drill50_phonebook_func.center_window(self, 500, 300)
         self.master.title("The Tkinter Phonebook Demo")
         self.master.configure(bg="#F0F0F0")
-        # This protocol method is a tkinter built-in method to catch if 
+        # This protocol method is a tkinter built-in method to catch if
         # the user clicks the upper corner, "X" on Windows OS.
-        self.master.protocol("WM_DELETE_WINDOW", lambda: drill50_phonebook_func.ask_quit(self))
+        self.master.protocol("WM_DELETE_WINDOW",
+                             lambda: drill50_phonebook_func.ask_quit(self))
         arg = self.master
 
-        # load in the GUI widgets from a separate module, 
+        # load in the GUI widgets from a separate module,
         # keeping your code comparmentalized and clutter free
         drill50_phonebook_gui.load_gui(self)
-        
+
         # Instantiate the Tkinter menu dropdown object
         # This is the menu that will appear at the top of our window
         menubar = Menu(self.master)
         filemenu = Menu(menubar, tearoff=0)
         filemenu.add_separator()
-        filemenu.add_command(label="Exit", underline=1,accelerator="Ctrl+Q",command=lambda: drill50_phonebook_func.ask_quit(self))
+        filemenu.add_command(label="Exit", underline=1, accelerator="Ctrl+Q",
+                             command=lambda: drill50_phonebook_func.ask_quit(self))
         menubar.add_cascade(label="File", underline=0, menu=filemenu)
-        helpmenu = Menu(menubar, tearoff=0) # defines the particular drop down colum and tearoff=0 means do not separate from menubar
+        # defines the particular drop down colum and tearoff=0 means do not separate from menubar
+        helpmenu = Menu(menubar, tearoff=0)
         helpmenu.add_separator()
         helpmenu.add_command(label="How to use this program")
         helpmenu.add_separator()
-        helpmenu.add_command(label="About This Phonebook Demo") # add_command is a child menubar item of the add_cascde parent item
-        menubar.add_cascade(label="Help", menu=helpmenu) # add_cascade is a parent menubar item (visible heading)
+        # add_command is a child menubar item of the add_cascde parent item
+        helpmenu.add_command(label="About This Phonebook Demo")
+        # add_cascade is a parent menubar item (visible heading)
+        menubar.add_cascade(label="Help", menu=helpmenu)
         """
             Finally, we apply the config method of the widget to display the menu
             From here we could also pass in additional aprams for additional 
@@ -81,7 +88,7 @@ class ParentWindow(Frame):
         """
         self.master.config(menu=menubar, borderwidth='1')
 
-        
+
 """
     It is from these few lines of code that Python will begin our gui and application
     The (if __name__ == "__main__":) part is basically telling Python that if this script
